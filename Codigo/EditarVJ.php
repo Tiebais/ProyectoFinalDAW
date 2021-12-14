e2cfb1d8901c039dfcaf88403117b464801709e5
Tiebais <95413498+Tiebais@users.noreply.github.com>
2021-12-14 22:14:07 +0100

    Update EditarVJ.php

--- Codigo/EditarVJ.php
+++ Codigo/EditarVJ.php
@@ -28,7 +28,7 @@
                     <h1 class="col-8 text-center">Editar Videojuego</h1>
 
                     <?php
-                        $conexion = conectar(true);
+                        $conexion = conectar(false);
                         $idVideojuego=($_GET['idVideojuego']);
                         $consulta= infoVideojuegosEdit($conexion, $idVideojuego);
                         while($fila=mysqli_fetch_array($consulta)){
