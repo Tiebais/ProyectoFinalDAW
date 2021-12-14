9d41cd08b7c3c1f7356cb902ea7a8c94ccc185a8
Tiebais <95413498+Tiebais@users.noreply.github.com>
2021-12-14 21:46:06 +0100

    Update Avideoconsolas.php

--- Codigo/admin/Avideoconsolas.php
+++ Codigo/admin/Avideoconsolas.php
@@ -28,7 +28,7 @@
     </thead>
     <tbody>
     <?php
-        $conexion = conectar(false);
+        $conexion = conectar(true);
         $consulta= consultarPlataforma($conexion);
         while($fila=mysqli_fetch_array($consulta)){
     ?>
