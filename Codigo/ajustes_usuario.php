85d027aa5944130c45ed1fd2985b0f587e48b740
Tiebais <95413498+Tiebais@users.noreply.github.com>
2021-12-14 22:19:58 +0100

    Update ajustes_usuario.php

--- Codigo/ajustes_usuario.php
+++ Codigo/ajustes_usuario.php
@@ -28,7 +28,7 @@
                     <h1 class="col-8 text-center">Ajustes de usuario</h1>
 
                     <?php
-                        $conexion = conectar(true);
+                        $conexion = conectar(false);
                         $idUsuario=($_SESSION['idUsuario']);
                         $consulta= EditarUsuarioFromulario($conexion, $idUsuario);
                         while($fila=mysqli_fetch_array($consulta)){
