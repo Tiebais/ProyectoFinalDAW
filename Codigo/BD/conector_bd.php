a87aeb664328be6559d1f59e5798de83f8eedcf6
Tiebais <95413498+Tiebais@users.noreply.github.com>
2021-12-14 23:36:33 +0100

    Update conector_bd.php

--- Codigo/BD/conector_bd.php
+++ Codigo/BD/conector_bd.php
@@ -5,21 +5,15 @@
 			$servidor = "...";
 			
 		} else {
-			$servidor = "127.0.0.1";
+			$servidor = "localhost";
 			$usuario = "debianDB";
 			$password = "debianDB";
 			$bd = "tienda_online";
 		}
 
 		//para establever una conexcion con una bd necesitamos usar la funcion mysqli_connect();
-		$conector = mysqli_connect($servidor, $usuario, $password, $bd, 3306);
-        	if ($conexion) {
-            	     return $conexion;
-        	}
-        	else{
-            	     echo mysqli_connect_error();
-        	}
-		
+		$conexion = mysqli_connect($servidor,$usuario,$password,$bd);
+
 		if ($conexion){
 			return $conexion;
 		} else {
