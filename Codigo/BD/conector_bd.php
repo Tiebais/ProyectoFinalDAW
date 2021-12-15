68c6f9645bcf040e2dfaa837a7b9a0becae94966
Tiebais <95413498+Tiebais@users.noreply.github.com>
2021-12-15 11:09:32 +0100

    Update conector_bd.php

--- Codigo/BD/conector_bd.php
+++ Codigo/BD/conector_bd.php
@@ -1,25 +1,18 @@
 <?php
-	
+class conexion {
 	function conectar($esRemota){
-		if ($esRemota){
-			$servidor = "...";
-			
-		} else {
-			$servidor = "localhost";
-			$usuario = "debianDB";
-			$password = "debianDB";
-			$bd = "tienda_online";
+		$database = 'mysql:dbname=tienda_online;host=localhost';
+		$usurio = 'debianDB';
+		$password = 'debianDB';
+		try {
+		   $base = new PDO($dbs, $usuario, $password);
+		   echo "Conexion exitosa <br>";
+		   return $base;
 		}
-
-		//para establever una conexcion con una bd necesitamos usar la funcion mysqli_connect();
-		$conexion = mysqli_connect($servidor,$usuario,$password,$bd);
-
-		if ($conexion){
-			return $conexion;
-		} else {
-			echo "Error no se ha podido conectar con la BD <br>";
-			//funcion que indica el error al conectar
-			echo mysqli_connect_error();
+		catch (PDOException $e) {
+		   echo 'Falló la conexion: ' . $e->getMessage();
 		}
 	}
+}
+
 ?>
