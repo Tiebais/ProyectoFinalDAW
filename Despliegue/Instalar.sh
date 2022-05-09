da990d98f3d384ffc2540c5a776c61d2bf52859f
Tiebais <95413498+Tiebais@users.noreply.github.com>
2022-05-10 01:13:56 +0200

    Update Instalar.sh

--- Despliegue/Instalar.sh
+++ Despliegue/Instalar.sh
@@ -13,8 +13,7 @@ WWW="/var/www/html/"
 DATOS="tienda_online.sql"
 BBDD="tienda_online"
 # Se toman los parámetros como USER y PASS de la BBDD
-if [ $# = 2 ];
-then
+if [ $# = 2 ]; then
    USERDB=$1
    PASSDB=$2
 fi
