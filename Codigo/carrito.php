1937ebb2fb6e61076ff00d5248ad5a8b812d3131
Tiebais <95413498+Tiebais@users.noreply.github.com>
2022-05-23 01:13:05 +0200

    Update carrito.php

--- Codigo/carrito.php
+++ Codigo/carrito.php
@@ -144,7 +144,7 @@
               <div class="col-md-7">
                 <div class="row">
                   <div class="col-md-12 text-right border-bottom mb-5">
-                    <h3 class="text-black h4 text-uppercase">Cart Totals</h3>
+                    <h3 class="text-black h4 text-uppercase">PRECIO</h3>
                   </div>
                 </div>
                 <div class="row mb-5">
@@ -156,10 +156,7 @@
                   </div>
                 </div>
 
-                <div class="row">
-                  <div class="col-md-12">
-                    <button class="btn btn-primary btn-lg py-3 btn-block" onclick="window.location='checkout.php'">Proceed To Checkout</button>
-              </div>
+                
             </div>
           </div>
         </div>
